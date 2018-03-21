c863df420d12d769b73e7213efaf46b4db86239e
Jakubem <jakubmx@gmail.com>
2018-03-21 22:23:25 +0100

    minify service worker script

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
 self.addEventListener('install', function(event) {
-    // We pass a promise to event.waitUntil to signal how 
-    // long install takes, and if it failed
     event.waitUntil(
-      // We open a cache…
       caches.open('decision-making-system').then(function(cache) {
-        // And add resources to it
         return cache.addAll([
           '/',
           'index.html',
@@ -15,16 +11,8 @@ self.addEventListener('install', function(event) {
   });
 
   self.addEventListener('fetch', function(event) {
-    // Calling event.respondWith means we're in charge
-    // of providing the response. We pass in a promise
-    // that resolves with a response object
     event.respondWith(
-      // First we look for something in the caches that
-      // matches the request
       caches.match(event.request).then(function(response) {
-        // If we get something, we return it, otherwise
-        // it's null, and we'll pass the request to
-        // fetch, which will use the network.
         return response || fetch(event.request);
       })
     );
