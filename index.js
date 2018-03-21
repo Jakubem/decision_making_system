20abe323ca6cd2e7d5681ae9a8d329277f2673ea
Jakubem <jakubmx@gmail.com>
2018-03-21 23:24:19 +0100

    remove unused console log

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ btn.addEventListener('click', function () {
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      console.log('Service worker registration succeeded:', registration);
     }).catch(function(error) {
       console.log('Service worker registration failed:', error);
     });
